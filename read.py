c91bc67052a6b2d6fb66bca4f2729d48cc3f391c
karen lin <yoyo861207@gmail.com>
2019-10-06 20:32:10 +0800

    review

---
+++ read.py
@@ -0,0 +1,9 @@
+data = []
+count = 0
+with open ('reviews.txt','r')as f :
+	for line in f :
+		data.append(line)
+		count+= 1
+		if count % 1000 == 0
+		    print(len(data))
+print('檔案讀取完了,總共有', len(data),'筆資料')
