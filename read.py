271a1451f147cb231e78f032ef5ce5d12f5a989b
karen lin <yoyo861207@gmail.com>
2019-10-06 21:14:46 +0800

    calc

--- read.py
+++ read.py
@@ -4,6 +4,11 @@ with open ('reviews.txt','r')as f :
 	for line in f :
 		data.append(line)
 		count+= 1
-		if count % 1000 == 0
+		if count % 1000 == 0 :
 		    print(len(data))
 print('檔案讀取完了,總共有', len(data),'筆資料')
+
+sum_len = 0
+for d in data :
+	sum_len = sum_len + len(d)
+print('留言的平均長度為',sum_len / len(data))
